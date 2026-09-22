037e409e0ec43f6a24beaeeae03846a480105de0
Yayasan Al-Fityah <marbotji@gmail.com>
2026-09-22 15:52:22 +0700

    fix: include ketua tahfidzh enum

--- supabase/migrations/202609220010_program_teacher_roles.sql
+++ supabase/migrations/202609220010_program_teacher_roles.sql
@@ -1,2 +1,3 @@
 alter type public.app_role add value if not exists 'GURU_TPA';
 alter type public.app_role add value if not exists 'GURU_TAHFIDZH';
+alter type public.app_role add value if not exists 'KETUA_TAHFIDZH';
